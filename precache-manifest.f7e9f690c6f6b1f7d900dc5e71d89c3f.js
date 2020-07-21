57d252074881e292b442799bc122a763ac44d5ba
Robertas Liekis <robertasliekis@gmail.com>
2020-07-21 16:33:51 +0300

    Updates

---
+++ precache-manifest.f7e9f690c6f6b1f7d900dc5e71d89c3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c07e0edbaab5f6840d9e0cc38f7709",
+    "url": "/hero-search/index.html"
+  },
+  {
+    "revision": "527eea269f821adf7306",
+    "url": "/hero-search/static/css/main.a7c4c94d.chunk.css"
+  },
+  {
+    "revision": "5b99631916d7caffb6d7",
+    "url": "/hero-search/static/js/2.42b06f48.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/hero-search/static/js/2.42b06f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527eea269f821adf7306",
+    "url": "/hero-search/static/js/main.0cf892f9.chunk.js"
+  },
+  {
+    "revision": "09c15eb158f5cf0aeb31",
+    "url": "/hero-search/static/js/runtime-main.cc00b252.js"
+  },
+  {
+    "revision": "a1409b3f30706b751bdacb252efc5f3d",
+    "url": "/hero-search/static/media/marvel-logo.a1409b3f.png"
+  }
+]);
